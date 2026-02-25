24d063456e037798b937d65735c3529358857f36
zhiwuli <398147488@qq.com>
2026-02-25 22:41:38 +0800

    first commit

---
+++ src/main/java/com/zhiwu/project2026/Project2026Application.java
@@ -0,0 +1,13 @@
+package com.zhiwu.project2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhiwu/project2026/Project2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhiwu.project2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
